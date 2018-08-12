7972f9e6e0c20490b0c6a847a6ece9d16402bfee
Flammchen <l.woehler@tu-braunschweig.de>
2018-08-12 12:05:46 +0200

    gui test buttons

--- button.lua
+++ button.lua
@@ -9,19 +9,26 @@ function Clickable:create(x, y, width, height)
     clickable.width = width
     clickable.height = height
     clickable.hovered = false
+    clickable.fixed = false
 
     return clickable
 end
 
 
 function Clickable:mousepressed(x, y, button, istouch, presses)
-    if (x + pos) >= self.x and (x + pos) < self.x + self.width and y >= self.y and y < self.y + self.height then
+    if not self.fixed then
+        x = x + pos
+    end
+    if x >= self.x and x < self.x + self.width and y >= self.y and y < self.y + self.height then
         self:clicked(button, istouch, presses)
     end
 end
 
 function Clickable:mousemoved(x,y, dx, dy, istouch)
-    if (x + pos) >= self.x and (x + pos) < self.x + self.width and y >= self.y and y < self.y + self.height then
+    if not self.fixed then
+        x = x + pos
+    end
+    if x >= self.x and x < self.x + self.width and y >= self.y and y < self.y + self.height then
         self.hovered = true
     else
         self.hovered = false
--- gui.lua
+++ gui.lua
@@ -1,3 +1,4 @@
+require "button"
 Gui = {}
 Gui.__index = Gui
 
@@ -5,21 +6,79 @@ function Gui:create()
     local gui = {}
     setmetatable(gui, Gui)
 
+    gui.current_tab = 2
+ 
+    gui.push_clickables = {}   
+    gui.tab1_clickable = Clickable:create(1400,300, 250, 100)
+    gui.tab2_clickable = Clickable:create(1400+250,300, 250, 100)
+    gui.tab1_clickable.fixed = true
+    gui.tab2_clickable.fixed = true
+
+    function gui.tab1_clickable:draw()
+        if gui.current_tab == 1 then
+            love.graphics.setColor(255,0,255)
+        love.graphics.rectangle("fill", 1400+pos, 300, 250, 100)
+        else 
+            love.graphics.setColor(255,0,255)
+        love.graphics.rectangle("line", 1400+pos, 300, 250, 100)
+        end
+    end
+
+    function gui.tab1_clickable:clicked()
+        gui.current_tab = 1
+    end
+
+    function gui.tab2_clickable:draw()
+        if gui.current_tab == 2 then
+            love.graphics.setColor(0,0,0)
+        love.graphics.rectangle("fill", 1400+pos + 250, 300, 250, 100)
+        else 
+            love.graphics.setColor(0,0,0)
+        love.graphics.rectangle("line", 1400+pos + 250, 300, 250, 100)
+        end
+    end
+
+    function gui.tab2_clickable:clicked()
+        gui.current_tab = 2
+    end
+
+    table.insert(gui.push_clickables, gui.tab1_clickable)
+    table.insert(gui.push_clickables, gui.tab2_clickable)
     return gui
 end
 
 function Gui:draw(num_people)
     local oldr, oldg, oldb = love.graphics.getColor()
     love.graphics.setColor(0,255,255)
---    love.graphics.rectangle("fill", 50 + pos, 50, 1000, 200)
-    --love.graphics.setColor(255,255,255)
-    --love.graphics.rectangle("fill", 1050 + pos, 50, 900, 200)
     love.graphics.setColor(0,0,255)
+    -- platzhalter stationen uebersicht
     love.graphics.rectangle("fill", 1100 + pos, 50, 250, 200)
     love.graphics.setColor(0,0,0)
+
+    -- print current money and time
     love.graphics.print("Money " .. money, 1400 + pos, 50)
     love.graphics.print(string.format("time %02d:%02d", time_h, time_m), 1400+pos, 150)
     love.graphics.print("People at this platform: " .. num_people, 50 + pos, 0)
+
+    -- menu
+    love.graphics.setColor(255,255,255)
     love.graphics.rectangle("fill", 1400+pos, 300, 500, 750)
+    love.graphics.setColor(0,0,0)
+    love.graphics.rectangle("line", 1400+pos, 300, 500, 750)
     love.graphics.setColor(oldr, oldg, oldb)
+    
+    self.tab1_clickable:draw()
+    self.tab2_clickable:draw()
+end
+
+function Gui:mousemoved(x, y, dx, dy, istouch)
+    for i, clickable in pairs(self.push_clickables) do
+        clickable:mousemoved(x, y, dx, dy, istouch)
+    end
+end
+
+function Gui:mousepressed(x, y, button, istouch, presses)
+    for i, clickable in pairs(self.push_clickables) do
+        clickable:mousepressed(x, y, button, istouch, presses)
+    end
 end
